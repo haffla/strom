8583b8095d7d9f8084c5d2cc56cf609be52c8324
Jakob Pupke <jakop@pm.me>
2020-05-18 10:33:24 +0200

    try fixing socket

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
-const app = require("express")();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const url = require("url");
 const next = require("next");
 
-const port = parseInt(process.env.PORT, 10) || 3000;
+const socketIO = require("socket.io");
+const http = require("http");
+
 const dev = process.env.NODE_ENV !== "production";
-const nextApp = next({ dev });
-const handle = nextApp.getRequestHandler();
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-io.on("connect", (socket) => {
-  socket.emit("now", { message: "hello" });
-});
+const requestListener = (req, res) => {
+  // Be sure to pass `true` as the second argument to `url.parse`.
+  // This tells it to parse the query portion of the URL.
+  const parsedUrl = url.parse(req.url, true);
+  handle(req, res, parsedUrl);
+};
 
-nextApp.prepare().then(() => {
-  app.all("*", (req, res) => {
-    return handle(req, res);
+app.prepare().then(() => {
+  const port = parseInt(process.env.PORT || "3000", 10);
+
+  const server = http.createServer(requestListener);
+
+  io = socketIO(server);
+  io.on("connection", (socket) => {
+    socket.emit("now", { message: "hello" });
+    socket.on("disconnect", () => {
+      console.log(socket.id, "disconnected");
+    });
   });
 
-  server.listen(port, (err) => {
-    if (err) throw err;
+  server.listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`);
   });
 });

fd24e34adbb9868e123d44fd30116fa0d7397d73
Jakob Pupke <jakop@pm.me>
2020-05-22 12:01:33 +0200

    cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/main.css";
-import { useEffect } from "react";
 import { StateProvider } from "../store";
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    console.log("HU");
-  });
-
   return (
     <StateProvider>
       <Component {...pageProps} />;
